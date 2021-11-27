3aa346395d743908e3b9e90488b8b1703389b0a7
DmitriyBartenev <bartenevdmitriy@bk.ru>
2021-11-27 15:06:58 +0300

    commit at 15:06

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-console.log('Hello world!');
-console.log('Here I am');
-console.log('Omegalul');
+let button1 = document.getElementByClass('button1')
+button1.addEventListener('click', smoothscroll)
+
+function smoothscroll(){
+    formaSend.scrollIntoView({behavior: 'smooth'})
+}
+//               TABS                //
+//$(document).ready(function(){
+//    $('.container_of_tabs').on('click', '.tab', function(){
+//        $('.container_of_tabs').find('active').removeClass('active');
+//        $(this).addClass('active');
+//        $('.tab-form').eq($(this).index()).addClass('active');
+//   });
+//});
